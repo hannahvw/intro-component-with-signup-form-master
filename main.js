c8c7f02c811097fd1ef4a1094181dd1bb438f846
hannahvw <103385894+hannahvw@users.noreply.github.com>
2024-04-28 14:34:28 -0500

    Updated js form validation

--- main.js
+++ main.js
@@ -14,39 +14,54 @@ function handleSubmit(event) {
   if (firstName.value === "") {
     lastName.style.marginTop = "35px";
     errorMsgFirstName.style.display = "block";
+    firstName.style.border = "2px solid var(--color-primary-red)";
   } else {
     errorMsgFirstName.style.display = "none";
     lastName.style.marginTop = "10px";
+    firstName.style.border = "1px solid hsl(245, 54%, 88%)";
   }
 
   if (lastName.value === "") {
     email.style.marginTop = "35px";
     errorMsgLastName.style.display = "block";
+    lastName.style.border = "2px solid var(--color-primary-red)";
   } else {
     errorMsgLastName.style.display = "none";
     email.style.marginTop = "10px";
+    lastName.style.border = "1px solid hsl(245, 54%, 88%)";
   }
 
   if (email.value === "" || !email.value.includes("@")) {
     password.style.marginTop = "35px";
     errorMsgEmail.style.display = "block";
+    email.style.border = "2px solid var(--color-primary-red)";
   } else {
     errorMsgEmail.style.display = "none";
     password.style.marginTop = "10px";
+    email.style.border = "1px solid hsl(245, 54%, 88%)";
   }
 
   if (password.value === "") {
     trialBtn.style.marginTop = "35px";
     errorMsgPassword.style.display = "block";
+    password.style.border = "2px solid var(--color-primary-red)";
   } else {
     errorMsgPassword.style.display = "none";
     trialBtn.style.marginTop = "10px";
+    password.style.border = "1px solid hsl(245, 54%, 88%)";
   }
 
-  firstName.value = "";
-  lastName.value = "";
-  email.value = "";
-  password.value = "";
+  if (
+    firstName.value !== "" &&
+    lastName.value !== "" &&
+    password.value !== "" &&
+    (email.value !== "" && email.value.includes("@"))
+  ) {
+    firstName.value = "";
+    lastName.value = "";
+    email.value = "";
+    password.value = "";
+  }
 }
 
 trialBtn.addEventListener("click", handleSubmit);
